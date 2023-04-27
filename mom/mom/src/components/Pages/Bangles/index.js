8675286246f24cc977e8c36b1878f9acb5c15519
Dharanikativarapu <kativarapudharani@gmail.com>
2023-04-27 17:34:00 +0530

    BasicCard adding

--- mom/mom/src/components/Pages/Bangles/index.js
+++ mom/mom/src/components/Pages/Bangles/index.js
@@ -1,7 +1,24 @@
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 export default function Bangles(){
     return(
         <>
-        fashion
-        </>
+        <Box sx={{ flexGrow: 1 }}>
+     <Grid container spacing={0}>
+       <Grid xs={12} sx={{backgroundColor:"red"}}>
+        gold
+       </Grid>
+       <Grid xs={12} sx={{backgroundColor:"pink"}}>
+        silver 
+       </Grid>
+       <Grid xs={12} sx={{backgroundColor:"lime"}}>
+        designing
+       </Grid>
+       <Grid xs={12} sx={{backgroundColor:"skyblue"}}>
+        diamond
+       </Grid>
+     </Grid>
+   </Box>
+   </>
     )
 }
--- mom/mom/src/components/Pages/Rings/index.js
+++ mom/mom/src/components/Pages/Rings/index.js
@@ -1,7 +1,41 @@
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import BasicCard from '../../BasicCard';
 export default function Rings(){
     return(
-        <>
-        hello
+        
+          <>
+         <Box sx={{ flexGrow: 1 }}>
+      
+        <Grid xs={12} sx={{backgroundColor:"purple"}}>
+         gold
+         <Grid container spacing={0}>
+         <BasicCard/>
+         <BasicCard/>
+        </Grid>
+        <Grid xs={12} sx={{backgroundColor:"red"}}>
+         silver 
+         <Grid container spacing={0}>
+         <BasicCard/>
+         <BasicCard/>
+        </Grid>
+        <Grid xs={12} sx={{backgroundColor:"pink"}}>
+         platinum 
+         <Grid container spacing={0}>
+         <BasicCard/>
+         <BasicCard/>
+        </Grid>
+        <Grid xs={12} sx={{backgroundColor:"skyblue"}}>
+         diamond
+         <Grid container spacing={0}>
+         <BasicCard/>
+         <BasicCard/>
+        </Grid>
+        </Grid>
+        </Grid>
+        </Grid>
+      </Grid>
+    </Box>
         </>
     )
 }
